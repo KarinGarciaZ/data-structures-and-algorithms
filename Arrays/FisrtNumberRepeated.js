5f01acbc0b3f24e5c6b8af3881f85ba3d688ca92
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-10-01 20:56:45 -0500

    First Number Repeated problem resolved with arrays and hash tables.

---
+++ Arrays/FisrtNumberRepeated.js
@@ -0,0 +1,27 @@
+let arr1 = [2, 5, 1, 2, 3, 55, 1, 2, 4]
+
+//O(n^2)
+const returnRepeated = arr => {
+  for (let i = 1; i < arr.length; i++) 
+    for (let j = i - 1; j >= 0; j--)      
+      if( arr[i] === arr[j] )
+        return arr[i]  
+
+  return undefined
+}
+
+//O(n)
+const returnRepeated2 = arr => {
+  let tempArray = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if ( tempArray[arr[i]] ) 
+      return arr[i]
+      
+    tempArray[arr[i]] = arr[i]    
+  }    
+
+  return undefined
+}
+console.log(returnRepeated(arr1))
+console.log(returnRepeated2(arr1))
---
+++ Hash Tables/FirstNumberRepeated.js
@@ -0,0 +1,46 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size)
+  }
+
+  _hash(key) {
+    let hash = 0
+    for (let i = 0; i < key.length; i++) 
+      hash = (key * (i + 1)) % this.data.length          
+    return hash
+  }
+
+  get(key) {
+    let address = this._hash(key)
+
+    if(this.data[address])
+      for (let i = 0; i < this.data[address].length; i++)
+        if( this.data[address][i][1] === key )
+          return key      
+
+    return null
+  }
+
+  set(key, value) {  
+    let address = this._hash(key)  
+    if(!this.data[address]) 
+      this.data[address] = []
+    
+    this.data[address].push([key, value])            
+  }
+}
+
+let arr1 = [2, 5, 1, 2, 3, 55, 1, 2, 4]
+
+const myHashTable = new HashTable(arr1.length)
+
+for (let i = 0; i < arr1.length; i++) {
+  let valueRepeated = myHashTable.get(arr1[i])
+
+  if( valueRepeated ) {
+    console.log(valueRepeated)
+    break
+  }
+
+  myHashTable.set(arr1[i], arr1[i])
+}
