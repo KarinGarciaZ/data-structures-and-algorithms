f816a4c1d5e3c8aa849b67ed28823b5f74d3f376
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-10-24 00:41:47 -0500

    Added nice solution to merge trees.

--- Trees/MergeTwoBinaryTrees.js
+++ Trees/MergeTwoBinaryTrees.js
@@ -42,8 +42,7 @@ class Tree {
   function mergeTrees(t1, t2) {
 
     if( !t1 && !t2 ) return null
-    if( t1 && !t2 ) return t1
-    if( !t1 && t2 ) return t2
+    if (!t1 || !t2)  return t1 || t2
 
     let lastNode = null
 
@@ -74,6 +73,24 @@ class Tree {
 
     return t1
   }
+
+  //A chinese solution xv
+  var mergeTrees = function(t1, t2) {
+    if (!t1 && !t2) {
+        return null;
+    }
+    
+    if (!t1 || !t2) {
+        return t1 || t2;
+    }
+    
+    var root = new TreeNode(t1.val + t2.val);
+    
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+    
+    return root;
+};
 
 
 let tree = new Tree()
