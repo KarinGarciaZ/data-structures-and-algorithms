5ddb75b88f1ed1a61b873fcb1517b8ad987713ab
Oscar Jovanny Garcia Zepeda <oscarin962011@hotmail.com>
2019-11-12 22:59:15 -0600

    Added functions to reverse string with recurtion.

--- Arrays/ReverseString.js
+++ Arrays/ReverseString.js
@@ -26,6 +26,29 @@ let reverse2 = str => {
 
 let reverse3 = str => [...str].reverse().join('')
 
+let reverse4 = str => {
+    let i = str.length - 1
+
+    let traverse = (i) => {
+        if(i < 0)
+            return ''
+        return str[i]+ traverse(i-1)
+    }
+    return traverse(i)
+}
+
+function reverseStringRecursive (str) {
+    console.log('====='+str)
+    if (str === "") {
+      return "";
+    } else {
+      return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+    }
+  }
+
+
 console.log(reverse('Hola Oscar'))
 console.log(reverse2('Hola Oscar'))
 console.log(reverse3('Hola Oscar'))
+console.log(reverse4('Hola Oscar'))
+console.log(reverseStringRecursive('Hola Oscar'))
